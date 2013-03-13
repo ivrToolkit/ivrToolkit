948b6ba446584715d5b3e0401e924fe359b9633b
tmakaro <troy@makarosoft.com>
2013-03-12 20:08:09 -0700

    Add an out line example

---
+++ Examples/OutTest/MainForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace OutTest
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDial = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.Location = new System.Drawing.Point(90, 42);
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtPhoneNumber.TabIndex = 0;
+            this.txtPhoneNumber.Text = "2502486892";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Phone Number:";
+            // 
+            // btnDial
+            // 
+            this.btnDial.Location = new System.Drawing.Point(90, 69);
+            this.btnDial.Name = "btnDial";
+            this.btnDial.Size = new System.Drawing.Size(75, 23);
+            this.btnDial.TabIndex = 2;
+            this.btnDial.Text = "Dial";
+            this.btnDial.UseVisualStyleBackColor = true;
+            this.btnDial.Click += new System.EventHandler(this.btnDial_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 264);
+            this.Controls.Add(this.btnDial);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPhoneNumber);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPhoneNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDial;
+    }
+}
+
---
+++ Examples/OutTest/MainForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ivrToolkit.Core;
+
+namespace OutTest
+{
+    public partial class MainForm : Form
+    {
+        private ILine line;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            line = LineManager.getLine(1);
+        }
+
+        private void btnDial_Click(object sender, EventArgs e)
+        {
+            CallAnalysis result = line.dial(txtPhoneNumber.Text, 3500);
+            Console.WriteLine(result);
+        }
+    }
+}

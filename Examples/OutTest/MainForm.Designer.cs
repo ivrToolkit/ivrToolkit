634b4866e177523d8caac326fa4af2632cc7d459
tmakaro <troy@makarosoft.com>
2013-03-25 15:27:15 -0700

    Remove my phone number

--- Examples/OutTest/MainForm.Designer.cs
+++ Examples/OutTest/MainForm.Designer.cs
@@ -35,40 +35,44 @@
             // 
             // txtPhoneNumber
             // 
-            this.txtPhoneNumber.Location = new System.Drawing.Point(90, 42);
+            this.txtPhoneNumber.Location = new System.Drawing.Point(120, 52);
+            this.txtPhoneNumber.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtPhoneNumber.Name = "txtPhoneNumber";
-            this.txtPhoneNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtPhoneNumber.Size = new System.Drawing.Size(132, 22);
             this.txtPhoneNumber.TabIndex = 0;
-            this.txtPhoneNumber.Text = "2502486892";
+            this.txtPhoneNumber.Text = "1112223333";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 45);
+            this.label1.Location = new System.Drawing.Point(4, 55);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.Size = new System.Drawing.Size(107, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "Phone Number:";
             // 
             // btnDial
             // 
-            this.btnDial.Location = new System.Drawing.Point(90, 69);
+            this.btnDial.Location = new System.Drawing.Point(120, 85);
+            this.btnDial.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnDial.Name = "btnDial";
-            this.btnDial.Size = new System.Drawing.Size(75, 23);
+            this.btnDial.Size = new System.Drawing.Size(100, 28);
             this.btnDial.TabIndex = 2;
             this.btnDial.Text = "Dial";
             this.btnDial.UseVisualStyleBackColor = true;
             this.btnDial.Click += new System.EventHandler(this.btnDial_Click);
             // 
-            // Form1
+            // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 264);
+            this.ClientSize = new System.Drawing.Size(379, 325);
             this.Controls.Add(this.btnDial);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtPhoneNumber);
-            this.Name = "Form1";
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "MainForm";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);

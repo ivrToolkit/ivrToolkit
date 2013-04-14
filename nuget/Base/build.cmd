76eff4752998ada935b05d4a85fe1534cc548c8b
tmakaro <troy.makaro@srb-es.com>
2013-04-14 14:43:38 -0700

    fix the build procedure

--- nuget/Base/build.cmd
+++ nuget/Base/build.cmd
@@ -1,10 +1,11 @@
 mkdir lib\net40
-mkdir tools
+mkdir tools\net40
 mkdir content
 
 xcopy /E/Y "..\..\ivrToolkit.Core\System Recordings\*.*" "content\System Recordings\"
 copy ..\..\examples\SimulatorTest\Voice.Properties content
 copy ..\..\README.md content
+copy ..\..\ivrToolkit.Core\install.ps1 tools\net40
 
 copy ..\..\ivrToolkit.Core\bin\debug\ivrToolkit.core.* lib\net40\
 copy ..\..\ivrToolkit.DialogicPlugin\bin\debug\ivrToolkit.dialogicPlugin.* lib\net40\

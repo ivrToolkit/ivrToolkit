9dfb391a529b3dc2bd7f4f5323f31099a64d48f7
tmakaro <troy@makarosoft.com>
2015-02-24 18:11:38 -0800

    Add more flexability for the outtest

--- Examples/OutTest/MainForm.Designer.cs
+++ Examples/OutTest/MainForm.Designer.cs
@@ -31,47 +31,63 @@
             this.txtPhoneNumber = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.btnDial = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLine = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // txtPhoneNumber
             // 
-            this.txtPhoneNumber.Location = new System.Drawing.Point(120, 52);
-            this.txtPhoneNumber.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPhoneNumber.Location = new System.Drawing.Point(133, 66);
             this.txtPhoneNumber.Name = "txtPhoneNumber";
-            this.txtPhoneNumber.Size = new System.Drawing.Size(132, 22);
+            this.txtPhoneNumber.Size = new System.Drawing.Size(100, 20);
             this.txtPhoneNumber.TabIndex = 0;
-            this.txtPhoneNumber.Text = "1112223333";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(4, 55);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(46, 69);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(107, 17);
+            this.label1.Size = new System.Drawing.Size(81, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Phone Number:";
             // 
             // btnDial
             // 
-            this.btnDial.Location = new System.Drawing.Point(120, 85);
-            this.btnDial.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnDial.Location = new System.Drawing.Point(133, 93);
             this.btnDial.Name = "btnDial";
-            this.btnDial.Size = new System.Drawing.Size(100, 28);
+            this.btnDial.Size = new System.Drawing.Size(75, 23);
             this.btnDial.TabIndex = 2;
             this.btnDial.Text = "Dial";
             this.btnDial.UseVisualStyleBackColor = true;
             this.btnDial.Click += new System.EventHandler(this.btnDial_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Line Number:";
+            // 
+            // txtLine
+            // 
+            this.txtLine.Location = new System.Drawing.Point(133, 40);
+            this.txtLine.Name = "txtLine";
+            this.txtLine.Size = new System.Drawing.Size(27, 20);
+            this.txtLine.TabIndex = 4;
+            this.txtLine.Text = "1";
+            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(379, 325);
+            this.ClientSize = new System.Drawing.Size(284, 138);
+            this.Controls.Add(this.txtLine);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.btnDial);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtPhoneNumber);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MainForm";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -85,6 +101,8 @@
         private System.Windows.Forms.TextBox txtPhoneNumber;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnDial;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLine;
     }
 }
 

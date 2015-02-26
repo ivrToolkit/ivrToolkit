26052919430d2f4981b3283d0294403020af9d81
MichaelGCox <Michael.Cox@SRB-ES.com>
2015-02-26 13:46:08 -0800

    The Dialogic SIP CLR Wrapper.  This provides a simple method of invoking the Dialogic SIP Library from Managed code such as C#

---
+++ ivrToolkit.DialogicSipWrapperSync/DialogicWrapperSync.h
@@ -0,0 +1,44 @@
+// DialogicWrapperSync.h
+#pragma once
+
+#include "../ivrToolkit.DialogicSipLibrary/hmp_sip.h"
+#include "../ivrToolkit.DialogicSipLibrary/hmp_sip.cpp"
+
+using namespace System;
+
+namespace DialogicWrapperSync {
+	/*
+	* This class only provides methods that are syncronous.  This helps prevent any confusion between 
+	* using syncornous and asyncronous features of the wrapper.
+	*/
+	public ref class DialogicSIPSync
+	{
+	public:
+		// constructor
+		DialogicSIPSync();
+		// destructor cleans up managed resources
+		~DialogicSIPSync();
+		//finalizer cleans up unmanaged resources
+		!DialogicSIPSync();
+
+		//wrapper methods
+
+		void WOpen(int channel_index);
+		void WClose();
+		void WStop();
+		void WRegister(String^ proxy_ip, String^ local_ip, String^ alias, String^ password, String^ realm);
+		void WUnregister();
+		void WStatus();
+		int WMakeCall(String^ ani, String^ dnis);
+		int WWaitCall();
+		void WDropCall();
+		String^ WGetDeviceName();
+		int WGetVoiceHandle();
+		int WGetCallState();
+
+	private:
+		DialogicFunctions *dialogicFunctions;
+		int channel_index;
+
+	};
+}

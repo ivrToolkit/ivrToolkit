635b08d084575cae2bda94cb8f4b67844c67104b
MichaelGCox <Michael.Cox@SRB-ES.com>
2015-02-26 13:52:44 -0800

    A very simple IVR console application.  This is used to test that the SIP plugin is working correctly for the IVR toolkit.

---
+++ Examples/ConsoleIvr/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ivrToolkit.Core;
+using ivrToolkit.Core.Util;
+using ConsoleIvr.ScriptBlocks;
+
+namespace ConsoleIvr
+{
+    class Program
+    {
+
+        private static ILine line;
+
+        static void Main(string[] args)
+        {
+
+            WaitCall();
+            //MakeCall();
+        }
+
+        static void MakeCall()
+        {
+            line = LineManager.GetLine(1);
+            CallAnalysis result = line.Dial("7782320255", 3500);
+            line.PlayFile("System Recordings\\Thursday.wav");
+            line.Hangup();
+            Console.WriteLine("End of Program");
+        }
+
+        static void WaitCall()
+        {
+
+
+            line = LineManager.GetLine(1);
+            Console.WriteLine("Got a Line");
+
+            line.WaitRings(2);
+
+            try
+            {
+
+                ScriptManager manager = new ScriptManager(line, new WelcomeScript());
+
+                while (manager.HasNext())
+                {
+                    // execute the next script
+                    manager.Execute();
+                }
+
+                line.Hangup();
+
+            }
+            catch (ivrToolkit.Core.Exceptions.HangupException)
+            {
+                line.Hangup();
+
+            }
+            line.Close();
+        }
+    }
+}
---
+++ Examples/ConsoleIvr/ScriptBlocks/WelcomeScript.cs
@@ -0,0 +1,29 @@
+﻿// 
+// Copyright 2013 Troy Makaro
+// 
+// This file is part of ivrToolkit, distributed under the LESSER GNU GPL. For full terms see the included COPYING file and the COPYING.LESSER file.
+// 
+// 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ivrToolkit.Core;
+
+namespace ConsoleIvr.ScriptBlocks
+{
+    public class WelcomeScript : AbstractScript
+    {
+        public override string Description
+        {
+            get { return "Welcome"; }
+        }
+
+        public override IScript Execute()
+        {
+            // say My welcome message
+            Line.PlayFile(@"Voice Files\ThankYou.wav");
+            return new MainScript();
+        }
+    } // class
+}

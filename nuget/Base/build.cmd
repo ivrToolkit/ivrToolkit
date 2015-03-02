c7f161707f897cf0dfec0f2a39bfe3a31bab0c18
tmakaro <troy@makarosoft.com>
2015-03-02 10:25:23 -0800

    missed the core library

--- nuget/Base/build.cmd
+++ nuget/Base/build.cmd
@@ -9,8 +9,8 @@ copy ..\..\README.md content
 copy ..\..\ReleaseNotes.md content
 copy ..\..\ivrToolkit.Core\install.ps1 tools\net40
 
-copy ..\..\ivrToolkit.Core\bin\debug\ivrToolkit.core.* lib\net40\
-copy ..\..\ivrToolkit.DialogicPlugin\bin\debug\ivrToolkit.dialogicPlugin.* lib\net40\
+copy ..\..\ivrToolkit.Core\bin\x86\debug\ivrToolkit.core.* lib\net40\
+copy ..\..\ivrToolkit.DialogicPlugin\bin\x86\debug\ivrToolkit.dialogicPlugin.* lib\net40\
 copy ..\..\ivrToolkit.SimulatorPlugin\bin\debug\ivrToolkit.SimulatorPlugin.* lib\net40\
 copy ..\..\ivrToolkit.DialogicSipPluginSync\bin\x86\Debug\ivrToolkit.DialogicSipPluginSync.* lib\net40\
 copy ..\..\Debug\ivrToolkit.DialogicSipLibrary.* lib\net40\

c74cebc8774156a34e0e850585f1a4d476c9aa6c
Troy Makaro <troy@makaro.com>
2025-01-26 21:00:53 -0800

    This fixes a bug in the dialogic sip plugin where the contact was being created incorrectly. Also the dialogic sip plugin used to support sip.local_ip so I had to put that back. Does not apply to the SipSorcery plugin

--- Plugins/Dialogic/Sip/ivrToolkit.Plugin.Dialogic.Sip/DialogicSipVoiceProperties.cs
+++ Plugins/Dialogic/Sip/ivrToolkit.Plugin.Dialogic.Sip/DialogicSipVoiceProperties.cs
@@ -2,70 +2,101 @@
 using Microsoft.Extensions.Logging;
 using System;
 
-namespace ivrToolkit.Plugin.Dialogic.Sip
+namespace ivrToolkit.Plugin.Dialogic.Sip;
+
+public class DialogicSipVoiceProperties : DialogicVoiceProperties, IDisposable
 {
-    public class DialogicSipVoiceProperties : DialogicVoiceProperties, IDisposable
+    private readonly ILogger _logger;
+
+    // Constants for property keys and defaults
+    private const string SIP_CHANNEL_OFFSET_KEY = "sip.channel_offset";
+    private const string SIP_CHANNEL_OFFSET_DEFAULT = "0";
+
+    private const string MAX_CALLS_KEY = "sip.max_calls";
+    private const string MAX_CALLS_DEFAULT = "1";
+
+    private const string SIP_SIGNALING_PORT_KEY = "sip.sip_signaling_port";
+    private const string SIP_SIGNALING_PORT_DEFAULT = "5060";
+
+    private const string SIP_PROXY_IP_KEY = "sip.proxy_ip";
+    private const string SIP_PROXY_IP_DEFAULT = "";
+
+    private const string SIP_LOCAL_IP_KEY = "sip.local_ip";
+    private const string SIP_LOCAL_IP_DEFAULT = "127.0.0.1";
+
+    private const string SIP_ALIAS_KEY = "sip.alias";
+    private const string SIP_ALIAS_DEFAULT = "";
+
+    private const string SIP_PASSWORD_KEY = "sip.password";
+    private const string SIP_PASSWORD_DEFAULT = "";
+
+    private const string SIP_REALM_KEY = "sip.realm";
+    private const string SIP_REALM_DEFAULT = "";
+
+    private const string SIP_CONTACT_KEY = "sip.contact";
+    private const string SIP_CONTACT_DEFAULT = "{alias}@{local_ip}:{sip_signaling_port}";
+
+    public DialogicSipVoiceProperties(ILoggerFactory loggerFactory, string fileName) : base(loggerFactory, fileName)
     {
-        private readonly ILogger _logger;
+        _logger = loggerFactory.CreateLogger<DialogicSipVoiceProperties>();
+        _logger.LogDebug("Ctr(ILoggerFactory loggerFactory, {0})", fileName);
+    }
 
-        public DialogicSipVoiceProperties(ILoggerFactory loggerFactory, string fileName) : base(loggerFactory, fileName)
+    /// <summary>
+    /// Number to add the line in order to get the channel.
+    /// </summary>
+    public uint SipChannelOffset => uint.Parse(GetProperty(SIP_CHANNEL_OFFSET_KEY, SIP_CHANNEL_OFFSET_DEFAULT));
+
+    public ushort MaxCalls => ushort.Parse(GetProperty(MAX_CALLS_KEY, MAX_CALLS_DEFAULT));
+
+    /// <summary>
+    /// The SIP port used for SIP signaling
+    /// </summary>
+    public ushort SipSignalingPort => ushort.Parse(GetProperty(SIP_SIGNALING_PORT_KEY, SIP_SIGNALING_PORT_DEFAULT));
+
+    /// <summary>
+    /// The SIP proxy ip address.  This is the address of the PBX that will be used to connect to the SIP Trunk.
+    /// </summary>
+    public string SipProxyIp => GetProperty(SIP_PROXY_IP_KEY, SIP_PROXY_IP_DEFAULT);
+    
+    /// <summary>
+    /// The local ip address.
+    /// </summary>
+    public string SipLocalIp => GetProperty(SIP_LOCAL_IP_KEY, SIP_LOCAL_IP_DEFAULT);
+
+    /// <summary>
+    /// The SIP account on the PBX server. This is the account that will be used to make and receive calls for this ADS SIP instance.
+    /// </summary>
+    public string SipAlias => GetProperty(SIP_ALIAS_KEY, SIP_ALIAS_DEFAULT);
+
+    /// <summary>
+    /// The SIP password for the SipAlias on the PBX server. 
+    /// </summary>
+    public string SipPassword => GetProperty(SIP_PASSWORD_KEY, SIP_PASSWORD_DEFAULT);
+
+    /// <summary>
+    /// The SIP realm for the SipAlias on the PBX server. 
+    /// </summary>
+    public string SipRealm => GetProperty(SIP_REALM_KEY, SIP_REALM_DEFAULT);
+
+    /// <summary>
+    /// The SIP contact
+    /// </summary>
+    public string SipContact
+    {
+        get
         {
-            _logger = loggerFactory.CreateLogger<DialogicSipVoiceProperties>();
-            _logger.LogDebug("Ctr(ILoggerFactory loggerFactory, {0})", fileName);
+            var result = GetProperty(SIP_CONTACT_KEY, SIP_CONTACT_DEFAULT);
+            result = result.Replace("{alias}", SipAlias)
+                .Replace("{local_ip}", SipLocalIp)
+                .Replace("{sip_signaling_port}", SipSignalingPort.ToString());
+            return result;
         }
+    }
 
-        /// <summary>
-        /// Number to add the line in order to get the channel.
-        /// </summary>
-        public uint SipChannelOffset => uint.Parse(GetProperty("sip.channel_offset", "0"));
-
-        public ushort MaxCalls => ushort.Parse(GetProperty("sip.max_calls", "1"));
-
-        /// <summary>
-        /// The SIP port used for SIP signaling
-        /// </summary>
-        public ushort SipSignalingPort => ushort.Parse(GetProperty("sip.sip_signaling_port", "5060"));
-
-        /// <summary>
-        /// The SIP proxy ip address.  This is the address of the PBX that will be used to connect to the SIP Trunk.
-        /// </summary>
-        public string SipProxyIp => GetProperty("sip.proxy_ip", "");
-
-        /// <summary>
-        /// The SIP account on the PBX server. This is the account that will be used to make and receive calls for this ADS SIP instance.
-        /// </summary>
-        public string SipAlias => GetProperty("sip.alias", "");
-
-        /// <summary>
-        /// The SIP password for the SipAlias on the PBX server. 
-        /// </summary>
-        public string SipPassword => GetProperty("sip.password", "");
-
-        /// <summary>
-        /// The SIP realm for the SipAlias on the PBX server. 
-        /// </summary>
-        public string SipRealm => GetProperty("sip.realm", "");
-
-        /// <summary>
-        /// The SIP contact
-        /// </summary>
-        public string SipContact
-        {
-            get
-            {
-                var result = GetProperty("sip.contact", "{alias}@{proxy_ip}:{sip_signaling_port}");
-                result = result.Replace("{alias}", SipAlias)
-                    .Replace("{proxy_ip}", SipProxyIp)
-                    .Replace("{sip_signaling_port}", SipSignalingPort.ToString());
-                return result;
-            }
-        }
-
-        public new void Dispose()
-        {
-            _logger.LogDebug("Dispose()");
-            base.Dispose();
-        }
-
+    public new void Dispose()
+    {
+        _logger.LogDebug("Dispose()");
+        base.Dispose();
     }
 }

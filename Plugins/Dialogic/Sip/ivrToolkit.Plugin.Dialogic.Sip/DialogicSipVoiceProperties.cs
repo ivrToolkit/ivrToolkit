4dd12d29b80824f759dbce6bca773a36831223fb
Troy Makaro <troy@makaro.com>
2025-01-26 19:43:56 -0800

    This fixes a bug in the dialogic sip plugin where the contact was being created incorrectly. Also the dialogic sip plugin used to support sip.local_ip so I had to put that back. Does not apply to the SipSorcery plugin

--- Plugins/Dialogic/Sip/ivrToolkit.Plugin.Dialogic.Sip/DialogicSipVoiceProperties.cs
+++ Plugins/Dialogic/Sip/ivrToolkit.Plugin.Dialogic.Sip/DialogicSipVoiceProperties.cs
@@ -21,6 +21,9 @@ public class DialogicSipVoiceProperties : DialogicVoiceProperties, IDisposable
     private const string SIP_PROXY_IP_KEY = "sip.proxy_ip";
     private const string SIP_PROXY_IP_DEFAULT = "";
 
+    private const string SIP_LOCAL_IP_KEY = "sip.local_ip";
+    private const string SIP_LOCAL_IP_DEFAULT = "127.0.0.1";
+
     private const string SIP_ALIAS_KEY = "sip.alias";
     private const string SIP_ALIAS_DEFAULT = "";
 
@@ -31,7 +34,7 @@ public class DialogicSipVoiceProperties : DialogicVoiceProperties, IDisposable
     private const string SIP_REALM_DEFAULT = "";
 
     private const string SIP_CONTACT_KEY = "sip.contact";
-    private const string SIP_CONTACT_DEFAULT = "{alias}@{proxy_ip}:{sip_signaling_port}";
+    private const string SIP_CONTACT_DEFAULT = "{alias}@{local_ip}:{sip_signaling_port}";
 
     public DialogicSipVoiceProperties(ILoggerFactory loggerFactory, string fileName) : base(loggerFactory, fileName)
     {
@@ -55,6 +58,11 @@ public class DialogicSipVoiceProperties : DialogicVoiceProperties, IDisposable
     /// The SIP proxy ip address.  This is the address of the PBX that will be used to connect to the SIP Trunk.
     /// </summary>
     public string SipProxyIp => GetProperty(SIP_PROXY_IP_KEY, SIP_PROXY_IP_DEFAULT);
+    
+    /// <summary>
+    /// The local ip address.
+    /// </summary>
+    public string SipLocalIp => GetProperty(SIP_LOCAL_IP_KEY, SIP_LOCAL_IP_DEFAULT);
 
     /// <summary>
     /// The SIP account on the PBX server. This is the account that will be used to make and receive calls for this ADS SIP instance.
@@ -84,7 +92,7 @@ public class DialogicSipVoiceProperties : DialogicVoiceProperties, IDisposable
         {
             var result = GetProperty(SIP_CONTACT_KEY, SIP_CONTACT_DEFAULT);
             result = result.Replace("{alias}", SipAlias)
-                .Replace("{proxy_ip}", SipProxyIp)
+                .Replace("{local_ip}", SipLocalIp)
                 .Replace("{sip_signaling_port}", SipSignalingPort.ToString());
             return result;
         }
